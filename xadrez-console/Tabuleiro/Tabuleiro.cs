6ba742a967ddc3c530320abc627f91617f2722b5
Mayke de Souza Nogueira <mayke.nogueira@grupomulti.com.br>
2024-07-02 19:59:40 -0300

    feat(exceptions):add aplication class for ex

--- xadrez-console/Tabuleiro/Tabuleiro.cs
+++ xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -15,10 +15,34 @@
         {
             return Pecas[linha, coluna];
         }
+        public Peca GetPeca(Posicao pos)
+        {
+            return Pecas[pos.Linha, pos.Coluna];
+        }
         public void InsertPeca(Peca peca, Posicao pos) 
         {
             Pecas[pos.Linha, pos.Coluna] = peca;
             peca.Posicao = pos;
+        }
+        public bool ExistPeca(Posicao posicao) 
+        {
+            ValidatePosition(posicao);
+            return GetPeca(posicao) != null;
+        }
+        public bool IsValidPosition(Posicao pos)
+        {
+            if (pos == null) return false;
+
+            if (pos.Linha > Linhas || pos.Linha < 0) { return false; }
+
+            if (pos.Coluna > Colunas|| pos.Coluna< 0) { return false; }
+
+            return true;
+        }
+
+        public void ValidatePosition(Posicao pos)
+        {
+            if(!IsValidPosition(pos)) throw new TabuleiroException("posição invalida!");
         }
     }
 }
---
+++ xadrez-console/Tabuleiro/TabuleiroException.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace tabuleiro
+{
+    internal class TabuleiroException : ApplicationException
+    {
+        public TabuleiroException(string message) : base(message) { }
+    }
+}
